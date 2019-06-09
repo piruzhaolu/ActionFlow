ebb5a04c759bdda31cbcec499c2b4cbf272a5623
piruzhaolu <liyuluan@foxmail.com>
2019-06-09 09:57:17 +0800

    LoopUntilFailure LoopUntilSuccess

---
+++ Packages/lipi.action-flow/Runtime/Nodes/BehaviorTree/BTLoopUntilFailureAsset.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ActionFlow
+{
+    /// <summary>
+    ///  子节点执行失败则返回成功，否则一直处理Running状态
+    /// </summary>
+    [System.Serializable]
+    public class BTLoopUntilFailure : StatusNodeBase<NullStatus>, IBehaviorCompositeNode
+    {
+        [NodeOutputBT]
+        public BehaviorStatus BehaviorInput(ref Context context)
+        {
+            var res = context.BTNodeOutput();
+            if (res == BehaviorStatus.Running)
+            {
+                context.Inactive(this);
+                return BehaviorStatus.Running;
+            }
+            else if (res == BehaviorStatus.Failure)
+            {
+                return BehaviorStatus.Success;
+            } else
+            {
+                context.Active(this);
+                return BehaviorStatus.Running;
+            }
+
+        }
+
+        public (bool, BehaviorStatus) Completed(ref Context context, int childIndex, BehaviorStatus result)
+        {
+            if(result == BehaviorStatus.Failure)
+            {
+
+                return (true, BehaviorStatus.Success);
+            } else if(result == BehaviorStatus.Success)
+            {
+                context.Active(this);
+                return (false, BehaviorStatus.None);
+            }
+            else
+            {
+                return (false, BehaviorStatus.None);
+                //throw new System.Exception("Child node completion cannot be Running ");
+            }
+        }
+
+        public override void OnTick(ref Context context)
+        {
+            var res = context.BTNodeOutput();
+            if (res == BehaviorStatus.Running)
+            {
+                context.Inactive(this);
+            } else if (res == BehaviorStatus.Failure)
+            {
+                context.Inactive(this);
+                context.BehaviorRunningCompleted(BehaviorStatus.Success);
+            }
+            else
+            {
+                context.Active(this);
+            }
+        }
+    }
+
+
+    [NodeInfo("BT/LoopUntilFailure")]
+    public class BTLoopUntilFailureAsset : NodeAsset<BTLoopUntilFailure>
+    {
+
+        
+    }
+}
