f56cd1cc5b9471a08c174853b373a50b9b374c34
piruzhaolu <liyuluan@foxmail.com>
2019-07-07 22:10:21 +0800

    NativeStaticArray

---
+++ Packages/lipi.action-flow/Runtime/Core/Container/NativeStaticArray.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Collections;
+using Unity.Entities;
+using System.Collections.Generic;
+
+namespace ActionFlow
+{
+    /// <summary>
+    /// 存储NativeStaticArrayHead描述的数据数组。主要就是存储在runtime才能确定数据类型（struct)和长度的数据
+    /// </summary>
+    public unsafe struct NativeStaticArray : IDisposable
+    {
+
+        
+        public static NativeStaticArray CreateMap(ref NativeStaticArrayHead head, int size)
+        {
+            var map = new NativeStaticArray
+            {
+                head = head,
+                Capacity = size,
+                data = (byte*)UnsafeUtility.Malloc(head.Size * size, 4, Allocator.Persistent)
+            };
+            return map;
+        }
+
+        private NativeStaticArrayHead head;
+        private byte* data;
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+
+
+        public NativeStaticArrayItem this[int i]
+        {
+            get
+            {
+                Debug.Assert(i < Count, "Index overflow");
+                return new NativeStaticArrayItem()
+                {
+                    data = data + head.Size * i,
+                    head = head
+                };
+            }
+        }
+
+        public int NewItem()
+        {
+            Count += 1;
+            if (Capacity < Count)
+            {
+                var newData = (byte*)UnsafeUtility.Malloc(head.Size * (Count + Capacity), 4, Allocator.Persistent);
+                UnsafeUtility.MemCpy(newData, data, Capacity * head.Size);
+                Capacity = Count + Capacity;
+                UnsafeUtility.Free(data, Allocator.Persistent);
+                data = newData;
+            }
+            return Count - 1;
+        }
+
+        public void Dispose()
+        {
+            head.Dispose();
+            UnsafeUtility.Free(data, Allocator.Persistent);
+        }
+
+    }
+
+    public unsafe struct NativeStaticArrayItem
+    {
+        internal byte* data;
+        internal NativeStaticArrayHead head;
+
+        public T Get<T>(int i) where T : struct
+        {
+            var pos = head[i];
+            UnsafeUtility.CopyPtrToStructure<T>(data + pos.Offset, out var v);
+            return v;
+        }
+
+        public void Set<T>(int i, T value) where T : struct
+        {
+            var pos = head[i];
+            UnsafeUtility.CopyStructureToPtr<T>(ref value, data + pos.Offset);
+        }
+    }
+}
---
+++ Packages/lipi.action-flow/Runtime/Core/Container/NativeStaticArrayHead.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using System;
+
+namespace ActionFlow
+{
+
+    /// <summary>
+    /// 静态的不同类型的Struct组成的数组
+    /// </summary>
+    public struct NativeStaticArrayHead:IDisposable
+    {
+        public struct TypePosition
+        {
+            //public int TypeIndex;
+            public int Offset;
+            public int Size;
+        }
+
+        internal NativeArray<TypePosition> TypePositions; //每位的类别数据
+
+        internal int Size; //总的内存大小
+
+
+        public TypePosition this[int i]
+        {
+            get
+            {
+                return TypePositions[i];
+            }
+        }
+
+
+        //public bool GetPosition<T>(out TypePosition pos) where T:struct
+        //{
+        //    var index = TypeManager.GetTypeIndex<T>();
+        //    return GetPosition(index, out pos);
+        //}
+
+        //public bool GetPosition(Type type, out TypePosition pos)
+        //{
+        //    var index = TypeManager.GetTypeIndex(type);
+        //    return GetPosition(index, out pos);
+        //}
+
+        //public bool GetPosition(int typeIndex, out TypePosition pos)
+        //{
+        //    for (int i = 0; i < TypePositions.Length; i++)
+        //    {
+        //        pos = TypePositions[i];
+        //        if (TypePositions[i].TypeIndex == typeIndex) return true;
+        //    }
+        //    pos = default;
+        //    return false;
+        //}
+
+
+        public void Dispose()
+        {
+            TypePositions.Dispose();
+        }
+
+        ///==========  Builder
+        ///==========  Builder
+        ///==========  Builder
+
+        public static Builder CreateBuilder()
+        {
+            var b = new Builder();
+            return b;
+        }
+
+        
+
+        public class Builder
+        {
+            internal NativeList<TypePosition> NativeLists;
+            private int offset;
+
+            public Builder()
+            {
+                NativeLists = new NativeList<TypePosition>(Allocator.Temp);
+                offset = 0;
+            }
+
+            public Builder Add<T>() where T : struct
+            {
+                var size = UnsafeUtility.SizeOf<T>();
+                NativeLists.Add(new TypePosition()
+                {
+                    Offset = offset,
+                    Size = size
+                });
+                offset += size;
+                return this;
+            }
+
+            //public Builder Add<T>(bool typeOnly = false) where T : struct
+            //{
+            //    var index = TypeManager.GetTypeIndex<T>();
+            //    if (typeOnly)
+            //    {
+            //        for (int i = 0; i < NativeLists.Length; i++)
+            //        {
+            //            if (NativeLists[i].TypeIndex == index) return this;
+            //        }
+            //    }
+
+            //    var size = UnsafeUtility.SizeOf<T>();
+            //    NativeLists.Add(new TypePosition()
+            //    {
+            //        TypeIndex = index,
+            //        Offset = offset,
+            //        Size = size
+            //    });
+            //    offset += size;
+            //    return this;
+            //}
+
+            public NativeStaticArrayHead ToHead()
+            {
+                var head = new NativeStaticArrayHead()
+                {
+                    TypePositions = NativeLists.ToArray(Allocator.Persistent),
+                    Size = offset
+                };
+                NativeLists.Dispose();
+                return head;
+            }
+
+
+        }
+
+    }
+
+
+    
+
+}
